0e2e46966b15df266ba767bcbc9a455e6cc157bd
Arvind Singh <vindar@users.noreply.github.com>
2021-08-25 01:49:22 +0200

    new examples for Teensy 3.5, 3.6, 4.0 and 4.1 with ST7735 160x128 screen

---
+++ examples/Teensy3/3D/characters/characters.ino
@@ -0,0 +1,228 @@
+/********************************************************************
+* tgx library example : displaying a 3D mesh...
+*
+* EXAMPLE FOR TEENSY 3.5 / 3.6 / 4 / 4.1
+*
+* WITH DISPLAY: ST7735 160x128 screen 
+********************************************************************/
+
+
+// 3D graphic library
+#include <tgx.h>
+#include <font_tgx_OpenSans_Bold.h>
+
+
+// screen driver library (bundled with teensyduino)
+#include <ST7735_t3.h> 
+
+
+#if defined(ARDUINO_TEENSY35)
+#define TWO_MODELS 0 // not enough flash memory to load both models
+#elif defined(ARDUINO_TEENSY36)
+#define TWO_MODELS 1
+#elif defined(ARDUINO_TEENSY40)
+#define TWO_MODELS 1
+#elif defined(ARDUINO_TEENSY41) 
+#define TWO_MODELS 1
+#else 
+#error This sketch only works on Teensy 3.5, 3.6, 4 or 4.1
+#endif
+
+
+
+// 3D models to draw
+#include "3Dmodels/stormtrooper/stormtrooper.h"
+#if TWO_MODELS
+#include "3DModels/cyborg/cyborg.h" // only for teensy 3.6 / 4 / 4.1
+#endif
+
+
+// do not bother with the tgx prefix
+using namespace tgx;
+
+
+
+// Pinout, hardware SPI0
+#define TFT_MOSI  11
+#define TFT_SCK   13
+#define TFT_DC   9 
+#define TFT_CS   10 
+#define TFT_RST  8
+
+
+// the screen driver: here using a small 160x128 pixels ST7735 screen
+ST7735_t3 tft = ST7735_t3(TFT_CS, TFT_DC, TFT_RST);
+
+// screen size (portrait mode)
+#define LX 128
+#define LY 160
+
+
+uint16_t fb1[LX * LY];      // framebuffer 1
+Image<RGB565> imfb1(fb1, LX, LY); // image that encapsulate framebuffer 1
+
+uint16_t fb2[LX * LY];      // framebuffer 2
+Image<RGB565> imfb2(fb2, LX, LY); // image that encapsulate framebuffer 2
+
+float zbuf[LX * LY]; // zbuffer
+
+Image<RGB565> * front_fb, * back_fb; 
+
+
+// the 3D mesh drawer (with zbuffer and perspective projection)
+Renderer3D<RGB565, LX, LY, true, false> renderer;
+
+
+void setup() 
+    {
+    front_fb = &imfb1;
+    back_fb = &imfb2;
+
+    // set up the screen driver
+    tft.initR(INITR_BLACKTAB);
+    tft.setRotation(0);
+    tft.setFrameBuffer(fb1); 
+    tft.useFrameBuffer(true);
+
+    // setup the 3D renderer.
+    renderer.setZbuffer(zbuf, LX * LY); // set the z buffer for depth testing
+    renderer.setPerspective(45, ((float)LX) / LY, 0.1f, 1000.0f);  // set the perspective projection matrix.     
+    renderer.setMaterial(RGBf(0.85f, 0.55f, 0.25f), 0.2f, 0.7f, 0.8f, 64); // bronze color with a lot of specular reflexion. 
+    renderer.setOffset(0, 0);
+    }
+
+
+
+
+/** Launch screen update and switch framebuffers **/
+void updateAndSwitchFB()
+    {
+    if (tft.asyncUpdateActive()) tft.waitUpdateAsyncComplete(); // wait for previous update to complete
+    tft.setFrameBuffer((uint16_t*)front_fb->data()); // set the framebuffer to upload
+    tft.updateScreen(); // launch DMA update; 
+    swap(front_fb, back_fb); // swap buffers
+    }
+
+
+/** Return the current framebuffer **/
+tgx::Image<tgx::RGB565>* currentFB()
+    {
+    return front_fb;
+    }
+
+
+/** Compute the model matrix according to the current time */  
+tgx::fMat4 moveModel(int & loopnumber) 
+    {    
+    const float end1 = 3000;
+    const float end2 = 2000;
+    const float end3 = 4000;
+    const float end4 = 2000;
+
+    int tot = (int)(end1 + end2 + end3 + end4);
+    int m = millis();
+
+    loopnumber = m / tot; 
+    float t = m % tot;
+
+    const float dilat = 9; // scale model
+    const float roty = 360 * (t / 4000); // rotate 1 turn every 4 seconds        
+    float tz, ty;
+    if (t < end1)
+        { // far away
+        tz = -25;
+        ty = 0;
+        }
+    else
+        {
+        t -= end1;
+        if (t < end2)
+            { // zooming in
+            t /= end2;
+            tz = -25 + 18 * t;
+            ty = -6.5f * t;
+            }
+        else
+            {
+            t -= end2;
+            if (t < end3)
+                { // close up
+                tz = -7;
+                ty = -6.5f;
+                }
+            else
+                { // zooming out
+                t -= end3;
+                t /= end4;
+                tz = -7 - 18 * t;
+                ty = -6.5f + 6.5f * t;
+                }
+            }
+        }
+
+    fMat4 M;
+    M.setScale({ dilat, dilat, dilat }); // scale the model
+    M.multRotate(-roty, { 0,1,0 }); // rotate around y
+    M.multTranslate({ 0,ty, tz }); // translate
+    return M;
+    }
+
+
+/** draw the current fps on the image */
+void fps()
+    {      
+    Image<RGB565> & im = *currentFB();
+    static elapsedMillis em = 0; // number of milli elapsed since last fps update
+    static int fps = 0;         // last fps 
+    static int count = 0;       // number of frames since the last update
+    // recompute fps every second. 
+    count++;
+    if ((int)em > 1000)
+        {
+        em = 0;
+        fps = count;
+        count = 0;
+        }
+    // display 
+    char buf[10];
+    sprintf(buf, "%d FPS", fps);
+    auto B = im.measureText(buf, { 0,0 }, font_tgx_OpenSans_Bold_9, false);
+    im.drawText(buf, { LX - B.lx() - 3,9 }, RGB565_Red, font_tgx_OpenSans_Bold_9,false);    
+    }
+
+    
+void loop() 
+    {
+    // compute the model position
+    int loopnumber;
+    fMat4  M = moveModel(loopnumber);
+    renderer.setModelMatrix(M);
+
+    // draw the 3D mesh
+    Image<RGB565> * im = currentFB();
+    renderer.clearZbuffer(); // clear the z-buffer
+    renderer.setImage(im); // set the image to draw onto
+    im->fillScreen(RGB565_Gray); // background
+
+    // choose the mesh
+#if (TWO_MODELS)
+    const Mesh3D<RGB565>* mesh = (loopnumber & 1) ? &stormtrooper : &cyborg; 
+#else
+    const Mesh3D<RGB565>* mesh = &stormtrooper; 
+#endif
+
+    renderer.drawMesh(TGX_SHADER_GOURAUD | TGX_SHADER_TEXTURE,mesh, false); // draw !
+
+    // flash memory just overflows on T3.6 when we try to print FPS :-( 
+    // (todo: fixit)  
+    #if not defined(ARDUINO_TEENSY36)  
+    fps();
+    #endif
+    
+    updateAndSwitchFB(); // update to screen and switch framebuffers
+
+    }
+
+
+/** end of file */
+
---
+++ examples/Teensy3/3D/scream/scream.ino
@@ -0,0 +1,317 @@
+/********************************************************************
+* tgx library example : changing the geometry at runtime. 
+* 
+* 'Explosion' the scream Painting !
+*
+* EXAMPLE FOR TEENSY 3.5 / 3.6 / 4 / 4.1
+*
+* WITH DISPLAY: ST7735 160x128 screen 
+********************************************************************/
+
+
+// 3D graphic library
+#include <tgx.h>
+#include <font_tgx_OpenSans_Bold.h>
+
+
+// screen driver library (bundled with teensyduino)
+#include <ST7735_t3.h> 
+
+// let's not burden ourselves with the tgx:: prefix
+using namespace tgx;
+
+// the texture image of the sheet
+#include "scream_texture.h"
+
+// Pinout, hardware SPI0
+#define TFT_MOSI  11
+#define TFT_SCK   13
+#define TFT_DC   9 
+#define TFT_CS   10 
+#define TFT_RST  8
+
+
+// the screen driver: here using a small 160x128 pixels ST7735 screen
+ST7735_t3 tft = ST7735_t3(TFT_CS, TFT_DC, TFT_RST);
+
+
+// screen size (landscape mode)
+#define LX 160
+#define LY 128
+
+uint16_t fb1[LX * LY];      // framebuffer 1
+Image<RGB565> imfb1(fb1, LX, LY); // image that encapsulate framebuffer 1
+
+uint16_t fb2[LX * LY];      // framebuffer 2
+Image<RGB565> imfb2(fb2, LX, LY); // image that encapsulate framebuffer 2
+
+float zbuf[LX * LY]; // zbuffer
+
+Image<RGB565> * front_fb, * back_fb; 
+
+     
+// the 3D mesh drawer (with zbuffer and perspective projection)
+Renderer3D<RGB565, LX, LY, true, false> renderer;
+
+static const int N = 30; // [-1,1]x[-1,1] is subdivided in NxM subsquares
+static const int M = 30; // total number of triangles is 2*N*M
+
+const float dx = 2.0f/N; // each subsquare has size  dx x dy
+const float dy = 2.0f/M;
+
+fVec3 vertices[(N+1)*(M+1)];        //  arrays of vertices : grid with y coordinate giving the height of the sheet at that point
+fVec3 normals[(N + 1) * (M + 1)];   // arrays of normals
+fVec2 texcoords[(N+1)*(M+1)];       // array of texture coordinates
+uint16_t faces[4*N*M];       // arrays of quads (in DMAMEM because when are running out of memory in DTCM).  
+
+
+/** Launch screen update and switch framebuffers **/
+void updateAndSwitchFB()
+    {
+    if (tft.asyncUpdateActive()) tft.waitUpdateAsyncComplete(); // wait for previous update to complete
+    tft.setFrameBuffer((uint16_t*)front_fb->data()); // set the framebuffer to upload
+    tft.updateScreen(); // launch DMA update; 
+    swap(front_fb, back_fb); // swap buffers
+    }
+
+
+/** Return the current framebuffer **/
+tgx::Image<tgx::RGB565>* currentFB()
+    {
+    return front_fb;
+    }
+
+
+/**
+* Initialise the vertices, texcoords and faces arrays
+**/
+void initSheet()
+    {
+    const float dx = 2.0f/N; 
+    const float dy = 2.0f/M;
+    for (int j = 0; j <= M; j++)
+        {
+        for (int i = 0; i <= N; i++)
+            {
+            vertices[(N + 1)*j + i] = fVec3(i*dx - 1, 0, j*dy - 1);
+            texcoords[(N + 1)*j + i] = fVec2(i*dx / 2, j*dy / 2);
+            }
+        }
+    for (int j = 0; j < M; j++)
+        {
+        for (int i = 0; i < N; i++)
+            {
+            faces[4 * (N * j + i) + 0] = (uint16_t)(j*(N+1) + i);
+            faces[4 * (N * j + i) + 1] = (uint16_t)((j + 1) * (N + 1) + i);
+            faces[4 * (N * j + i) + 2] = (uint16_t)((j+1)*(N+1) + i + 1);
+            faces[4 * (N * j + i) + 3] = (uint16_t)(j * (N + 1) + i + 1);
+            }
+        }
+    }
+
+
+/**
+* Compute the face normal of the CCW triangle with index i1, i2, i3
+* and add the vector to the normal vector of each of its vertices
+**/
+void faceN(int i1, int i2, int i3)
+    {
+    const fVec3 P1 = vertices[i1]; 
+    const fVec3 P2 = vertices[i2];
+    const fVec3 P3 = vertices[i3];
+    const fVec3 N = crossProduct(P1 - P3, P2 - P1);
+    normals[i1] += N;
+    normals[i2] += N;    
+    normals[i3] += N;
+    }
+
+
+/**
+* Recompute the normal arrays. 
+**/
+void updateNormals()
+    {
+    // set all normals to 0
+    for (int k = 0; k < (N + 1) * (M + 1); k++) normals[k] = fVec3(0, 0, 0);
+    // add the normal of the adjacent faces
+    for (int j = 0; j < M; j++)
+        {
+        for (int i = 0; i < N; i++)
+            {
+            faceN((N + 1) * j + i, (N + 1) * (j + 1) + i, (N + 1) * j + i + 1);
+            faceN((N + 1) * (j + 1) + i, (N + 1) * (j + 1) + i + 1, (N + 1) * j + i + 1);
+            }
+        }
+    // normalize the normals
+    for (int k = 0; k < (N + 1) * (M + 1); k++) normals[k].normalize();
+    }
+
+
+/** sine cardinal function (could maybe speedup with a precomputed table ?)*/
+float sinc(float x)
+    {
+    if (abs(x) < 0.01f) return 1.0f; 
+    return sinf(x)/x; 
+    }
+
+
+/** return uniform random number in [a,b[ */
+float unif(float a, float b)
+    {
+    const int step = 1000000;
+    return a + ((b - a) * random(step)) / step;
+    }
+
+
+/** return the camera's curent position (depending on time) */
+fVec3 cameraPosition()
+    {   
+    static elapsedMillis em; 
+    const float T = 30.0f; // rotation period (in seconds). 
+    const float d = 1.5 + 0.5*sin(em/10000.0f); // distance from sheet move closer / farther away with time. 
+    const float w = em * 2 * M_PI / (1000 * T) + M_PI*0.6f;
+    return fVec3(d * sinf(w), d*0.8f, d * cosf(w));
+    }
+
+
+/** draw the current fps on the image */
+void fps()
+    {      
+    Image<RGB565> & im = *currentFB();
+    static elapsedMillis em = 0; // number of milli elapsed since last fps update
+    static int fps = 0;         // last fps 
+    static int count = 0;       // number of frames since the last update
+    // recompute fps every second. 
+    count++;
+    if ((int)em > 1000)
+        {
+        em = 0;
+        fps = count;
+        count = 0;
+        }
+    // display 
+    char buf[10];
+    sprintf(buf, "%d FPS", fps);
+    auto B = im.measureText(buf, { 0,0 }, font_tgx_OpenSans_Bold_9, false);
+    im.drawText(buf, { LX - B.lx() - 3,9 }, RGB565_Red, font_tgx_OpenSans_Bold_9,false);    
+    }
+
+
+/** display an explosion */
+void explosion(fVec2 center, float h, float w, float s, float start_delay = 0)
+    {     
+    Serial.print("\n\nExplosion\n - position: ");
+    center.print();
+    Serial.printf(" - height  : %.2f\n - size    : %.2f\n - speed   : %2f\n\n", h, w, s);
+
+    s /= 1000.0f; // normalise speed
+
+    elapsedMillis et = 0; 
+
+    const float t0 = start_delay;          // time the bump starts to grow. 
+    const float t1 = start_delay + 2000;   // time of explosion 
+    const float t2 = start_delay + 5000;   // time when explosion is finished
+   
+    while (et < t2)
+        {
+        Image<RGB565> & im = *currentFB();
+        
+        float t = et; 
+
+        // erase the screen
+        im.fillScreen(RGB565_Black);
+
+        // clear the z-buffer
+        renderer.clearZbuffer();
+
+        // amplitude multiplier 
+        const float alpha = ((t <= t0) || (t >= t2)) ? 0 : ((t < t1) ? (t - t0) / (t1 - t0) : (1 - (t - t1) / (t2 - t1)));
+
+        // compute heights
+        for (int j = 0; j <= M; j++)
+            {
+            for (int i = 0; i <= N; i++)
+                {
+                const float x = dx*i - 1.0f - center.x;
+                const float y = dy*j - 1.0f - center.y;
+                const float r = sqrt(x * x + y * y); // distance to center. 
+                vertices[(N + 1) * j + i].y = alpha * h * sinc(w* (r - max(0, t - t1)*s));
+                }
+            }
+
+        // compute normals
+        updateNormals();
+
+        // increase time
+        t += 0.5f;
+
+        // set the camera position
+        renderer.setLookAt(cameraPosition(), { 0,0,0 }, { 0,1,0 });
+
+        // draw !
+        renderer.drawQuads(TGX_SHADER_GOURAUD | TGX_SHADER_TEXTURE, N * M, faces, vertices, faces, normals, faces, texcoords, &scream_texture);
+
+        // update the screen (asynchronous). 
+        fps();        
+        updateAndSwitchFB();
+        renderer.setImage(currentFB());
+        }
+    }
+
+
+void setup()
+    {
+    Serial.begin(9600);
+
+    front_fb = &imfb1;
+    back_fb = &imfb2;
+
+    // set up the screen driver
+    tft.initR(INITR_BLACKTAB);
+    tft.setRotation(1);
+    tft.setFrameBuffer(fb1); 
+    tft.useFrameBuffer(true);
+ 
+    // setup the 3D renderer.
+    renderer.setOffset(0, 0); //  image = viewport
+    renderer.setZbuffer(zbuf, LX * LY); // set the z buffer for depth testing    
+    renderer.setPerspective(45, ((float)LX) / LY, 0.1f, 1000.0f);  // set the perspective projection matrix. 
+    renderer.setCulling(0); // in case we see below the sheet. 
+    renderer.setImage(currentFB());
+            
+    fMat4 MV;
+    MV.setIdentity();
+    renderer.setModelMatrix(MV); // identity model matrix. 
+
+    // intialize the sheet. 
+    initSheet(); 
+
+    // the default seed give a nice sequence of explosion 
+    randomSeed(0);
+    }
+
+                  
+
+void loop()
+    {
+    // first an explosion carefully chosen...
+    const float h = 0.4;
+    const float w = 10;
+    const float s = 0.6;
+    fVec2 center(0,0);
+    explosion(center, h, w, s, 2000);
+
+    // ...and then random explosions
+    while (1)
+        { 
+        const float h = unif(0.0f, 0.3f);   // bump height
+        const float w = unif(3, 18);        // wave size
+        const float s = unif(0.2, 2);       // wave speed; 
+        fVec2 center(unif(-0.7, 0.7), unif(-0.7, 0.7));  // explosion center   
+        explosion(center,h,w,s);
+        }
+     }
+       
+
+/** end of file */
+
