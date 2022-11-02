308b1752ca58dd936b9155b320ae71490163fa2e
Arvind Singh <vindar@users.noreply.github.com>
2022-11-02 10:48:12 +0100

    blop

--- docs/examples.js
+++
@@ -1,4 +0,0 @@
-var examples =
-[
-    [ "D:/Programmation/tgx/src/Image.h", "_d_1_2_programmation_2tgx_2src_2_image_8h-example.html", null ]
-];
--- docs/structtgx_1_1_rasterizer_vec4.js
+++
@@ -1,6 +0,0 @@
-var structtgx_1_1_rasterizer_vec4 =
-[
-    [ "A", "structtgx_1_1_rasterizer_vec4.html#ab82d120b5f4431d7122efd6039f3727f", null ],
-    [ "color", "structtgx_1_1_rasterizer_vec4.html#a6bb3735fc8a11cdb526f2d6bfd1b8a3d", null ],
-    [ "T", "structtgx_1_1_rasterizer_vec4.html#ada42e59a35c4ed1c2e36b51fea9c4483", null ]
-];
