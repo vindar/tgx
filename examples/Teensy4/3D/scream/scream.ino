6a6dd0f4a509e5f119251c0dd947470a110de88c
Arvind Singh <vindar@users.noreply.github.com>
2026-05-28 09:45:13 +0200

    updated examples

--- examples/Teensy4/3D/scream/scream.ino
+++ examples/Teensy4/3D/scream/scream.ino
@@ -103,13 +103,7 @@ fVec2 texcoords[(N+1)*(M+1)];       // array of texture coordinates
 DMAMEM uint16_t faces[4*N*M];       // arrays of quads (in DMAMEM because when are running out of memory in DTCM).  
 
 
-// Print per-second FPS and frame timing on Serial.
-void telemetryBegin();
-void telemetryStartFrame();
-void telemetryEndFrame();
-
-void telemetrySetScene(const char* scene);
-
+#include "tgx_example_telemetry.h"
 
 /**
 * Initialise the vertices, texcoords and faces arrays
@@ -361,95 +355,3 @@ void loop()
 
         }
      }
-       
-
-// Print per-second FPS and frame timing on Serial.
-uint32_t telemetry_last_ms = 0;
-uint32_t telemetry_frame_start_us = 0;
-uint32_t telemetry_frames = 0;
-uint32_t telemetry_sum_us = 0;
-uint32_t telemetry_min_us = 0xFFFFFFFFu;
-uint32_t telemetry_max_us = 0;
-uint32_t telemetry_cycle = 0;
-const char* telemetry_scene = "startup";
-
-void telemetryBegin()
-    {
-    telemetry_last_ms = millis();
-    telemetry_frames = 0;
-    telemetry_sum_us = 0;
-    telemetry_min_us = 0xFFFFFFFFu;
-    telemetry_max_us = 0;
-    }
-
-
-void telemetryStartFrame()
-    {
-    if (telemetry_frames == 0) telemetry_last_ms = millis();
-    telemetry_frame_start_us = micros();
-    }
-
-
-static void telemetryPrintScene()
-    {
-    for (const char* p = telemetry_scene; *p != 0; p++)
-        {
-        const char c = *p;
-        Serial.print((c <= ' ' || c == '=') ? '_' : c);
-        }
-    }
-
-void telemetrySetScene(const char* scene)
-    {
-    if (scene == nullptr) scene = "unnamed";
-    telemetry_scene = scene;
-    telemetry_cycle++;
-    telemetry_last_ms = millis();
-    telemetry_frames = 0;
-    telemetry_sum_us = 0;
-    telemetry_min_us = 0xFFFFFFFFu;
-    telemetry_max_us = 0;
-    Serial.print("\n[TGX scene] cycle=");
-    Serial.print(telemetry_cycle);
-    Serial.print(" scene=");
-    telemetryPrintScene();
-    Serial.println();
-    }
-
-
-void telemetryEndFrame()
-    {
-    const uint32_t dt = micros() - telemetry_frame_start_us;
-    telemetry_frames++;
-    telemetry_sum_us += dt;
-    if (dt < telemetry_min_us) telemetry_min_us = dt;
-    if (dt > telemetry_max_us) telemetry_max_us = dt;
-
-    const uint32_t now = millis();
-    const uint32_t elapsed_ms = now - telemetry_last_ms;
-    if (elapsed_ms >= 1000)
-        {
-        Serial.print("\n[TGX telemetry] cycle=");
-        Serial.print(telemetry_cycle);
-        Serial.print(" scene=");
-        telemetryPrintScene();
-        Serial.print(" fps=");
-        Serial.print((1000.0f * telemetry_frames) / elapsed_ms, 2);
-        Serial.print(" frame_avg_us=");
-        Serial.print(((float)telemetry_sum_us) / telemetry_frames, 1);
-        Serial.print(" frame_min_us=");
-        Serial.print(telemetry_min_us);
-        Serial.print(" frame_max_us=");
-        Serial.print(telemetry_max_us);
-        Serial.print(" frames=");
-        Serial.println(telemetry_frames);
-
-        telemetry_last_ms = now;
-        telemetry_frames = 0;
-        telemetry_sum_us = 0;
-        telemetry_min_us = 0xFFFFFFFFu;
-        telemetry_max_us = 0;
-        }
-    }
-
-/** end of file */
